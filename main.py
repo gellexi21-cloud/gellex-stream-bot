0f507b6764b34fddb91013368f5442abd928a4a3
gellexi21-cloud <gellexi21@gmail.com>
2026-06-23 11:59:22 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import time
+import json
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHANNEL = os.getenv("TELEGRAM_CHANNEL")
@@ -35,51 +36,54 @@ def get_stream(token):
     )
 
     data = r.json().get("data", [])
-
-    if len(data) > 0:
-        return data[0]
-
-    return None
+    return data[0] if data else None
 
 
 def send_message(title, game):
-    text = (
-        f"🔴 Gelle_x вышла в эфир!\n\n"
-        f"🎮 {game}\n"
+    caption = (
+        f"🎀 Гелька Икс запустила стрим\n\n"
+        f"▫️ {game}\n"
         f"📝 {title}"
     )
 
-    r = requests.post(
-        f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage",
-        json={
-            "chat_id": TELEGRAM_CHANNEL,
-            "text": text,
-            "reply_markup": {
-                "inline_keyboard": [[
-                    {
-                        "text": "🎥 Смотреть стрим",
-                        "url": f"https://twitch.tv/{TWITCH_USERNAME}"
-                    }
-                ]]
+    reply_markup = {
+        "inline_keyboard": [[
+            {
+                "text": "Смотреть стрим✨",
+                "url": f"https://twitch.tv/{TWITCH_USERNAME}"
             }
-        },
-    )
+        ]]
+    }
+
+    with open("live.jpg", "rb") as photo:
+        r = requests.post(
+            f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendPhoto",
+            data={
+                "chat_id": TELEGRAM_CHANNEL,
+                "caption": caption,
+                "reply_markup": json.dumps(reply_markup),
+            },
+            files={
+                "photo": photo
+            },
+        )
 
     result = r.json()
-
     print("Telegram response:", result)
 
     return result["result"]["message_id"]
 
 
 def delete_message(msg_id):
-    requests.post(
+    r = requests.post(
         f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/deleteMessage",
         json={
             "chat_id": TELEGRAM_CHANNEL,
             "message_id": msg_id,
         },
     )
+
+    print("Delete response:", r.json())
 
 
 token = get_token()
@@ -104,7 +108,8 @@ while True:
         elif not stream and was_live:
             print("Stream ended")
 
-            delete_message(message_id)
+            if message_id:
+                delete_message(message_id)
 
             was_live = False
             message_id = None
